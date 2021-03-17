227112d10bbbc009f1c05758b80a55d5726f5684
Nishant Hulwan <nishanthulwan47@gmail.com>
2021-03-17 12:40:53 -0400

    Finishing project

--- list_content.py
+++ list_content.py
@@ -1,24 +1,25 @@
 import logging
 import argparse
 import os
+from datetime import datetime
 from collections import namedtuple
 _ntuple_diskusage = namedtuple('usage', 'total used free')
 
 
 def info_drive(drive_name):
-    logger = logging.getLogger("info_drive")
     logging.basicConfig()
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     # Declaring variables for holding number of files and directory number.
+    logger.info(datetime.now().strftime("%Y-%m-%dT%H:%M:%S") + " Drive name: " + drive_name)
+    logger.info(disk_usage(drive_name))
     directory_number = files_number = 0
     for _, dirnames, filenames in os.walk(drive_name):
         files_number += len(filenames)
         directory_number += len(dirnames)
-    logger.info("Drive name: " + drive_name)
-    logger.info(f"Number of directories: {directory_number}")
-    logger.info(f"Number of files: {files_number}")
-    logger.info(disk_usage(drive_name))
+    # Time stamps for log files
+    logger.info(datetime.now().strftime("%Y-%m-%dT%H:%M:%S") + f"Number of directories: {directory_number}")
+    logger.info(datetime.now().strftime("%Y-%m-%dT%H:%M:%S") + f"Number of files: {files_number}")
 
 
 def disk_usage(path):
@@ -31,20 +32,30 @@ def disk_usage(path):
     used = (st.f_blocks - st.f_bfree) * st.f_frsize
     return _ntuple_diskusage(total, used, free)
 
-def switch_arguments(argument):
-    switcher = {
-        1: "-d"
-    }
-    print switcher.get(argument, "Use -d to get list of directories and number of files")
+def show_help_message():
+    # Initializing logger
+    logging.basicConfig()
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.info("-d <drive name>: To display drive names, Total number of directories, Total number of files, Total "
+                "allocated, used, free storage")
+    logger.info("-l <folder name>: To list all the folders in a given drive with the following info")
+    logger.info("-f <list files>: To list all the files of the machine with file name, file type, file size"
+                "Date/Time Stamp of the file")
+    logger.info("-t <list types of files>: To list all the types of files exist in the machine")
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    # Help switch which will provide all possible switches and arguments
     parser.add_argument("-d", help="list all the drives in a a machines")
-    parser.add_argument("-h", help="Help Switch which will provide all possible switches and arguments")
+    parser.add_argument("-l", help="To list all the folders in a given drive")
+    parser.add_argument("-f", help="To list all the files of the machine")
+    parser.add_argument("-t", help="To list all the types of files exist in machine")
+
+
+
     if (parser.parse_args().d):
         driver_name = parser.parse_args().d
         info_drive(driver_name)
-    elif (parser.parse_args().h):
-        help_name = parser.parse_args().h
-        info_drive(help_name)
